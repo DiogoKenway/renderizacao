4f8dcaf5d609191d90539c4d27bed9c8230f1851
Diogo Alves <diogodidi2022@gmail.com>
2023-01-26 22:46:46 -0300

    generating a warning

--- pages/estatico1.jsx
+++ pages/estatico1.jsx
@@ -2,7 +2,7 @@ export default function estatico1() {
     return (
         <div>
             <h1>Estático #01</h1>
-            <h2>{Math.random()}</h2>
+            <h2>{Math.random()} </h2>
         </div>
     )
 }
