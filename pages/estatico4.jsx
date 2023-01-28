2ca43a0fbba10c4798fe016fb6a7a47d98918ebe
Diogo Alves <diogodidi2022@gmail.com>
2023-01-28 01:13:02 -0300

    end of file code

--- pages/estatico4.jsx
+++ pages/estatico4.jsx
@@ -1,16 +1,35 @@
-export function getStaticProps() {
+export async function getStaticProps() {
+
+    const response = await fetch("http://localhost:3000/api/produtos");
+    const produtos = await response.json()
+
+    console.log(produtos);
+
     return {
         props: {
-
+            produtos
         }
     }
 }   
 
 export default function estatico2(props) {
+    console.log(props);
     return (
         <div>
             <h1>Estatico #04</h1>
-        
+            <ul>
+                {
+                    props.produtos.map(produto => (
+                        <li key={produto.id}>
+                            <div>
+                            id: {produto.id},
+                            nome: {produto.nome},
+                            preço: {produto.preco}
+                            </div>
+                        </li>
+                    ))
+                }
+            </ul>
         </div>
     )
 } 
