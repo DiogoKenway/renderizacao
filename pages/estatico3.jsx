eb11b799ab0f90b13436132cd9a480844d8898b3
Diogo Alves <diogodidi2022@gmail.com>
2023-01-28 00:46:05 -0300

    adding new file

---
+++ pages/estatico3.jsx
@@ -0,0 +1,17 @@
+export async function getStaticProps() {
+    return {
+        props: {
+            revalidate: 7,
+            numero: Math.random()
+        }
+    }
+}
+
+export default function estatico2(props) {
+    return (
+        <div>
+            <h1>Estatico #03</h1>
+            <h2>{props.numero}</h2>
+        </div>
+    )
+} 
