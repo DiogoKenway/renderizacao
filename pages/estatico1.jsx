f4003d52519e9b391c2ddfd87509826ff48e29dc
Diogo Alves <diogodidi2022@gmail.com>
2023-01-27 23:10:36 -0300

    static content

--- pages/estatico1.jsx
+++ pages/estatico1.jsx
@@ -2,16 +2,16 @@
 
 export default function estatico1() {
 
-    // const [num, setNum] = useState(0);
+    const [num, setNum] = useState(0);
 
-    // useEffect(() => {
-    //     setNum(Math.random())
-    // },[])
+    useEffect(() => {
+        setNum(Math.random())
+    },[])
 
     return (
         <div>
             <h1>Estático #01</h1>
-            {/* <h2>{num}</h2> */}
+            <h2>{num}</h2>
         </div>
     )
 }
