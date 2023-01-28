6049c3de3da2ed8e569c55be8ad805dfebe09647
Diogo Alves <diogodidi2022@gmail.com>
2023-01-28 00:56:45 -0300

    adding new file

---
+++ pages/estatico4.jsx
@@ -0,0 +1,16 @@
+export function getStaticProps() {
+    return {
+        props: {
+
+        }
+    }
+}   
+
+export default function estatico2(props) {
+    return (
+        <div>
+            <h1>Estatico #04</h1>
+        
+        </div>
+    )
+} 
