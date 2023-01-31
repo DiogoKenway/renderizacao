7978292c77f56ec3296ee3c99b30f4a000a7aca0
Diogo Alves <diogodidi2022@gmail.com>
2023-01-30 22:22:16 -0300

    remove spacing

--- pages/estatico4.jsx
+++ pages/estatico4.jsx
@@ -13,7 +13,6 @@ export async function getStaticProps() { // método do servidor
 }   
 
 
-
 export default function estatico2(props) {
     console.log("[client] renderizando o componente...");
     return (
