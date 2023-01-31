c8d58f53b82a34b556f626978d0af2147a773817
Diogo Alves <diogodidi2022@gmail.com>
2023-01-31 19:45:23 -0300

    adding code

--- pages/dinamico1.jsx
+++ pages/dinamico1.jsx
@@ -1,7 +1,7 @@
 export default function Dinamico1(props) {
     return (
-    <div>
-        <h1>Dinâmico #01</h1>
-    </div>
+        <div>
+            <h1>Dinâmico #01</h1>
+        </div>
     )
 }
